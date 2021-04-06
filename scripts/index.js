b86d5a4539e1f49d22d24aa0ff26bf9587cff05d
hanielledann <hanielledann@gmail.com>
2021-04-06 17:16:16 -0400

    changes

---
+++ scripts/index.js
@@ -0,0 +1,67 @@
+// variables
+let body = document.querySelector(".page");
+
+let editButton = document.querySelector(".profile__edit-button");
+
+let closeButton = document.querySelector(".popup__close");
+
+let profileEditor = document.querySelector(".popup");
+
+let profileEditorForm = document.querySelector(".popup__form");
+
+let profileName = document.querySelector(".profile__name");
+
+let profileTitle = document.querySelector(".profile__title");
+
+let popupName = document.querySelector(".popup__input_role_name");
+
+let popupTitle = document.querySelector(".popup__input_role_title");
+
+// Retrieve profile object on page load
+let profile = {};
+updateProfile();
+
+// functions
+function updateProfile() {
+  profile = {
+    name: profileName.textContent,
+    title: profileTitle.textContent
+  }
+}
+
+function editProfile() {
+  updateProfile();
+  popupName.value = profile.name;
+  popupTitle.value = profile.title;
+  profileEditor.classList.add("popup_opened");
+
+  body.addEventListener('keyup', function escOut(e) {
+    if (e.key === 'Escape') {
+      closeEditor();
+    }
+  });
+}
+
+function saveProfile(e) {
+  e.preventDefault();
+
+  profileName.textContent = popupName.value;
+  profileTitle.textContent = popupTitle.value;
+
+  updateProfile();
+  closeEditor();
+}
+
+function closeEditor() {
+  profileEditor.classList.remove("popup_opened");
+}
+
+// event listeners
+editButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closeEditor);
+profileEditorForm.addEventListener('submit', saveProfile);
+
+
+
+
+
