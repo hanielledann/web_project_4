4663dacd71fd5151269f0777af58e980f5860011
hanielledann <hanielledann@gmail.com>
2021-04-23 15:27:50 -0400

    project5

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,143 @@
-let profileName = document.querySelector(".profile__name");
+const body = document.querySelector(".page");
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__title");
+const placesContainer = document.querySelector(".elements");
+const editButton = document.querySelector(".profile__edit-button");
+const closeProfileEditor = document.querySelector(".popup__close_role_edit");
+const profileEditor = document.querySelector(".popup_role_edit");
+const profileEditorForm = document.querySelector(".popup__form_role_edit");
+const popupName = document.querySelector(".popup__input_role_name");
+const popupTitle = document.querySelector(".popup__input_role_title");
+const addButton = document.querySelector(".profile__add-button");
+const closeImageAdder = document.querySelector(".popup__close_role_add");
+const newPlaceAdder = document.querySelector(".popup_role_add");
+const imageAdderForm = document.querySelector(".popup__form_role_add");
+const popupImageTitle = document.querySelector(".popup__input_role_image-title");
+const popupImageLink = document.querySelector(".popup__input_role_image-link");
+const imagePreview = document.querySelector(".popup_role_image");
+const closePreviewButton = document.querySelector(".popup__close_role_image");
+const popupImage = document.querySelector(".popup__image");
+const popupImageCaption = document.querySelector(".popup__caption");
+let profile = {};
+updateProfile();
 
-let profileTitle = document.querySelector(".profile__title");
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "./images/yosemite.png",
+  },
+  {
+    name: "Lake Louise",
+    link: "./images/lakelouise.png",
+  },
+  {
+    name: "Bald Mountains",
+    link: "./images/baldmountains.png",
+  },
+  {
+    name: "Latemar",
+    link: "./images/latemar.png",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "./images/vanois.png",
+  },
+  {
+    name: "Lago di Braies",
+    link: "./images/lago.png",
+  },
+];
 
-let editButton = document.querySelector(".profile__edit-button");
+initialCards.forEach((place) => {
+  addPlace(place.name, place.link);
+});
 
-let closeButton = document.querySelector(".popup__close");
-
-let profileEditor = document.querySelector(".popup");
-
-let profileEditorForm = document.querySelector(".popup__form");
-
-let popupName = document.querySelector(".popup__input_role_name");
-
-let popupTitle = document.querySelector(".popup__input_role_title");
-
-
-function editProfile() {
-  popupName.value = profileName.textContent;
-  popupTitle.value = profileTitle.textContent; 
-  profileEditor.classList.add("popup_opened");
+function updateProfile() {
+  profile = {
+    name: profileName.textContent,
+    title: profileTitle.textContent,
+  };
 }
 
-function closeEditor() {
-  profileEditor.classList.remove("popup_opened");
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  document.addEventListener("keydown", closeWithEscape);
+}
+
+function editProfile() {
+  updateProfile();
+  popupName.value = profile.name;
+  popupTitle.value = profile.title;
+  openPopup(profileEditor);
+}
+
+function openPlaceAdder() {
+  openPopup(newPlaceAdder);
+  popupImageTitle.value = "";
+  popupImageLink.value = "";
+}
+
+function createCard(title, link) {
+  const placeTemplate = document.querySelector("#place-template").content;
+  const newPlace = placeTemplate.querySelector(".element").cloneNode(true);
+  const deleteButton = newPlace.querySelector(".element__delete");
+  deleteButton.addEventListener("click", (e) =>
+    e.target.parentElement.remove()
+    );
+  const likeButton = newPlace.querySelector(".element__heart");
+  likeButton.addEventListener("click", (e) =>
+    e.target.classList.toggle("element__heart_liked")
+  );
+  const placeImage = newPlace.querySelector(".element__image");
+  placeImage.addEventListener("click", openPreview);
+  newPlace.querySelector(".element__name").textContent = title;
+  placeImage.src = link;
+  placeImage.alt = `${title}`;
+  return newPlace;
+}
+
+function addPlace(title, link) {
+  const newPlace = createCard(title, link);
+  placesContainer.prepend(newPlace);
+}
+
+function savePlace(e) {
+  e.preventDefault();
+  addPlace(popupImageTitle.value, popupImageLink.value);
+  closePopup();
 }
 
 function saveProfile(e) {
   e.preventDefault();
   profileName.textContent = popupName.value;
   profileTitle.textContent = popupTitle.value;
-  closeEditor();
+  updateProfile();
+  closePopup();
 }
 
-editButton.addEventListener('click', editProfile);
-closeButton.addEventListener('click', closeEditor);
-profileEditorForm.addEventListener('submit', saveProfile);
+function closePopup() {
+  const openPopup = document.querySelector(".popup_opened");
+  openPopup.classList.remove("popup_opened");
+  document.removeEventListener("keydown", closeWithEscape);
+}
 
-// Ah! I finally understand what you've been trying to get me to fix all along.
-// Thank you for pushing me till I got it right!! 
-// - Dan :)
+function openPreview(e) {
+  popupImage.src = e.target.src;
+  popupImage.alt = e.target.alt;
+  popupImageCaption.textContent = e.target.alt;
+  openPopup(imagePreview);
+}
+
+function closeWithEscape(e) {
+  if (e.key === "Escape") {
+    closePopup();
+  }
+}
+
+editButton.addEventListener("click", editProfile);
+addButton.addEventListener("click", openPlaceAdder);
+closeProfileEditor.addEventListener("click", closePopup);
+closeImageAdder.addEventListener("click", closePopup);
+closePreviewButton.addEventListener("click", closePopup);
+profileEditorForm.addEventListener("submit", saveProfile);
+imageAdderForm.addEventListener("submit", savePlace);
