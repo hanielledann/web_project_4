b8e9f982d2a9e5be45cec0261f469c221b81289b
hanielledann <hanielledann@gmail.com>
2021-04-23 15:45:54 -0400

    final changes

--- scripts/script.js
+++ scripts/script.js
@@ -24,29 +24,29 @@ updateProfile();
 const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "./images/yosemite.png",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
   },
   {
     name: "Lake Louise",
-    link: "./images/lakelouise.png",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
   },
   {
     name: "Bald Mountains",
-    link: "./images/baldmountains.png",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
   },
   {
     name: "Latemar",
-    link: "./images/latemar.png",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
   },
   {
     name: "Vanoise National Park",
-    link: "./images/vanois.png",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
   },
   {
     name: "Lago di Braies",
-    link: "./images/lago.png",
-  },
-];
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+]; 
 
 initialCards.forEach((place) => {
   addPlace(place.name, place.link);
